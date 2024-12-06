9fbaa7f060b988aa6cbff11319971c2ce3bd3073
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-06 14:08:46 +0100

    finished "Navigation and App Layout" page

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -2,6 +2,7 @@ package com.example.application.views.list;
 
 import com.example.application.data.Contact;
 import com.example.application.services.CrmService;
+import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
@@ -13,7 +14,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Contacts | Vaadin CRM")
-@Route("")
+@Route(value = "", layout = MainLayout.class)
 public class ListView extends VerticalLayout {
 
     Grid<Contact> grid = new Grid<>(Contact.class);
