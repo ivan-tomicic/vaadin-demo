b285a68dd366764905a6b34b1336868be532c494
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-06 15:17:12 +0100

    finished "Installation and offline" page

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,6 +1,8 @@
 package com.example.application;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +16,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
+@PWA(
+        name = "Vaadin CRM",
+        shortName = "CRM",
+        offlinePath = "offline.html",
+        offlineResources = {"images/logo.png", "images/empty-plant.png"})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
